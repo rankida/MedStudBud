cf8214c9cea15a00e7fa1fdcaecd0c6de24e16b3
David Rankin <Rankida@gmail.com>
2011-01-04 13:10:19 +0000

    Added initial rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MedStudyBuddy::Application.config.secret_token = '1e90b235f6112a49f209f70d062af4920c9b377f7415a080eec9505b61c24ff52e24613702bc89850df9172de20e8ecf0776985930c510635728695cb0b67663'
