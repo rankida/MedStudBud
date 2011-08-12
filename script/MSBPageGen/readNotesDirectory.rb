00e4e00de29fb5ada77875f7f7b0581513938439
David Rankin <Rankida@gmail.com>
2011-08-12 19:00:56 +0100

    Got a simple script to read the Notes Directory. Next tasks are to get it to generate a simple index file and then to do it recursivly.

--- script/MSBPageGen/readNotesDirectory.rb
+++ script/MSBPageGen/readNotesDirectory.rb
@@ -15,14 +15,24 @@ r = []
 Dir.foreach(dir_path) do |f|
   spec = Results.new
   spec.file_name = f
-  spec.is_dir = File.directory?(f)
+  spec.is_dir = File.directory?(dir_path + '/' + f)
   #spec.dir_exists = Dir.exist?(f)
-  spec.html = f.match(/\.html|$/i) || f.match(/\.htm|$/i)
+  spec.html = f.match(/\.html|$/i) != nil || f.match(/\.htm|$/i) != nil
   r.push spec
 end
 
+puts "Listing for " + dir_path
+puts
+puts
+
 longestName = r.max_by {|x| x.file_name.length }
-puts "max = " + longestName.file_name
+
+puts "   #{'File Name'.ljust(longestName.file_name.length)} |  Html  | is Dir |  |"
+puts "   #{'---------'.ljust(longestName.file_name.length, '-')} |  ----  | ------ | -|"
+
+r.each do |result|
+  puts " - #{result.file_name.ljust(longestName.file_name.length)} | #{result.html.to_s.ljust(6)} | #{result.is_dir.to_s.ljust(6)} | #{result.dir_exists} | "
+end
 
 
 
